d7248cf38598f82d1e2430497ae161b090bff349
rabee <rabee.dameer@gmail.com>
2018-10-16 09:43:05 +0200

    removed unused image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 // external
 import React, {Component, Fragment} from 'react';
-// import bgImg from '../images/pexels-photo-56759.jpeg';
+
+//internal
 import myImage from '../images/me.jpg';
 
 class Home extends Component {
@@ -14,7 +15,7 @@ class Home extends Component {
           <div className="row">
             <div className="col-lg-8">
               <h2 className="mt-5 display-4">
-                JUNIOR FULL-STACK WEB DEVELOPER
+                JUNIOR FRONT-END WEB DEVELOPER
               </h2>
             </div>
             <div className="col-lg-4 text-center"><img src={myImage} alt="rabee" width="350" height="350" className="rounded-circle"/>
