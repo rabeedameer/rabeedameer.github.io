66d9d46580ae51451a5e6bbaafd031acbd2d8b29
rabee <rabee.dameer@gmail.com>
2018-11-26 14:01:39 +0100

    updated info

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -17,10 +17,8 @@ class About extends Component {
         <p>
           Hi, I’m Rabee Dameer, a junior web developer with teaching and social work experiences. 
         </p>
-        <p>
-          After leaving my country and moving to Germany in 2015, my life was totally changed and I couldn't start working as a teacher again and also I have seen such a huge upward trend in tech and web development world. So without hesitation, I decided to have a new start with a new challenge. then the opportunity appeared at&nbsp; 
-          <abbr title="Digital Carrier Center">DCI </abbr>&nbsp;in Berlin and I could start my first step being a web developer.
-        </p>
+        <p>I arrived in Germany in 2015, since that time my life has totally changed and also I have seen such a huge upward trend in tech and web development world. So without hesitation, I decided to have a new start with a new challenge. Then the opportunity appeared at&nbsp; 
+            <abbr title="Digital Carrier Center">DCI </abbr>&nbsp;in Berlin and I could start my first step being a web developer. </p>
       </section>
       <More/>
       <Coding/>
